0415255f141c9a87524fa9fbedc6a2d9cbecd416
user <user@DESKTOP-HQHKDR5>
2025-02-05 00:38:01 +0900

    ''

---
+++ coco_animals_strict_subset/train.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# YOLOv11 Segmentation 모델 불러오기 (사전 학습된 모델)
+model = YOLO('yolo11n-seg.pt')
+
+# 모델 훈련 + 파인튜닝 (초기 레이어 동결)
+model.train(data='./coco_animals_strict_subset/data.yaml', epochs=50, imgsz=640, batch=16, freeze=10)
